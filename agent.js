93fbd23bfe4c7ca34222c574e136f2541f40c400
hubcarl <hubcarl@126.com>
2017-04-30 19:25:49 +0800

    fix:config

--- agent.js
+++ agent.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = agent => {
-  agent.logger.debug('agent start');
-  // require('./lib/service/client')(agent);
+  require('./lib/service/client')(agent);
   // require('./lib/service/server')(agent);
 };
 

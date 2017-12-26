190f8144dc74fca13669fbc51688a2a9819381c7
hubcarl <hubcarl@126.com>
2017-12-26 12:45:15 +0800

    fix:node client render single webpackConfig

--- agent.js
+++ agent.js
@@ -1,14 +1,19 @@
 'use strict';
+const Utils = require('./lib/utils');
 const WebpackServer = require('./lib/server');
 const MultProcessWebpackServer = require('./lib/mult-process-server');
 module.exports = agent => {
   agent.messenger.on('egg-ready', () => {
     const config = agent.config.webpack;
-    if (config.webpackConfigList && config.webpackConfigList.length) {
+    // 兼容 Node 前端渲染只有一个 webpack 配置
+    if (config.webpackConfigList && !Array.isArray(config.webpackConfigList)) {
+      config.webpackConfigList = [config.webpackConfigList];
+    }
+    if (Utils.isUseMultProcess(agent.baseDir, config)) {
+      new MultProcessWebpackServer(agent, config).start();
+    } else {
       config.useDll = config.useDll || false;
       new WebpackServer(agent, config).start();
-    } else {
-      new MultProcessWebpackServer(agent, config).start();
     }
   });
 };

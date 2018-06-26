ecfa4448c7a11922b8476e1b419106895e047a9b
wanghx <whx89768@alibaba-inc.com>
2018-06-26 18:59:22 +0800

    feat: get real port by egg-ready eventy

--- agent.js
+++ agent.js
@@ -3,8 +3,9 @@ const Utils = require('./lib/utils');
 const WebpackServer = require('./lib/server');
 const MultProcessWebpackServer = require('./lib/mult-process-server');
 module.exports = agent => {
-  agent.messenger.on('egg-ready', () => {
+  agent.messenger.on('egg-ready', ({ port }) => {
     const config = agent.config.webpack;
+    config.appPort = port;
     agent.messenger.setMaxListeners(config.maxListeners || 10000);
     // 兼容 Node 前端渲染只有一个 webpack 配置
     if (config.webpackConfigList && !Array.isArray(config.webpackConfigList)) {
